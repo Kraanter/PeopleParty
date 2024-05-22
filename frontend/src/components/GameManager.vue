ed4308cf06aeeacc225da90e586d2fb6c2d01276
Krijn Grimme <krijngrimme@hotmail.com>
2024-05-22 16:09:04 +0200

    Added Firefox support

--- frontend/src/components/GameManager.vue
+++ frontend/src/components/GameManager.vue
@@ -110,14 +110,7 @@ watch(
       id="container"
       class="bg-black backdrop-blur-xl bg-opacity-50 shadow-lg rounded-md w-full h-[97dvh]"
     >
-      <component
-        v-if="width && height"
-        :is="gameViewComp"
-        ref="gameViewRef"
-        :data="gameData"
-        :height
-        :width
-      />
+      <component v-if="width && height" :is="gameViewComp" ref="gameViewRef" :height :width />
     </div>
   </div>
 </template>
