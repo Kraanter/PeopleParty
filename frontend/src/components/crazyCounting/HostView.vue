6ca4acf30636e31f5b880408754132870b155d0d
Krijn Grimme <krijngrimme@hotmail.com>
2024-05-24 01:02:17 +0200

    Added locked in players list to crazycounting hostview

--- frontend/src/components/crazyCounting/HostView.vue
+++ frontend/src/components/crazyCounting/HostView.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { Application } from 'vue3-pixi'
+import { NCard, NScrollbar } from 'naive-ui'
 import { ref, toRefs, defineProps, computed } from 'vue'
 import TimeComponent from '../TimeComponent.vue'
 import {
@@ -8,8 +9,11 @@ import {
   GameStateType,
   MiniGamePayloadType
 } from '@/flatbuffers/messageClass'
+import { type Player } from '@/stores/confettiStore'
 
 const size = computed(() => appSize.value / 10)
+// TODO: Set locked in players from the data
+const lockedInPlayers = ref<Player[]>([])
 
 const props = defineProps<{
   width: number
@@ -46,7 +50,6 @@ const update = (data: MiniGamePayloadType) => {
       let localEntities: PosData[] = []
       for (let i = 0; i < hostEntitiesPayload.entitiesLength(); i++) {
         const entity = hostEntitiesPayload.entities(i)
-        console.log(entity?.xPos(), entity?.yPos())
         if (entity === null) continue
         localEntities.push(interpolatePosition(entity))
       }
@@ -64,8 +67,26 @@ defineExpose({
 </script>
 <template>
   <div class="flex justify-stretch">
-    <div class="mx-auto mt-4">
-      <TimeComponent :timeLeft />
+    <div class="mt-4 w-full h-full flex flex-col justify-center">
+      <div class="mx-auto mb-4">
+        <TimeComponent :timeLeft />
+      </div>
+      <p v-if="lockedInPlayers.length > 0" class="text-4xl w-full text-center text-white">
+        Awnsers locked:
+      </p>
+      <n-scrollbar class="mt-4">
+        <div
+          class="mx-auto mb-4 w-4/5"
+          v-for="(player, i) in lockedInPlayers.slice().reverse()"
+          :key="i"
+        >
+          <n-card>
+            <p class="font-bold text-2xl w-full text-center overflow-ellipsis">
+              {{ player.name }}
+            </p>
+          </n-card>
+        </div>
+      </n-scrollbar>
     </div>
     <Application key="gameview" :width="appSize" :height="appSize" background-color="white">
       <sprite
