0df9c2fa9a7cd4ef6942cfb2967fc8b22f9d054c
JelleD <jelledriessen2003@gmail.com>
2024-05-30 10:44:38 +0200

    Rewrote threadTimer class for better readability

--- backend/src/thread_timer.h
+++ backend/src/thread_timer.h
@@ -10,56 +10,66 @@
 
 class ThreadTimer
 {
+public:
+	ThreadTimer() : active(false) { }
+	~ThreadTimer() { clear(); }
+	void setInterval(std::function<void()> ptr, int timer);
+	void setTimeout(std::function<void()> ptr, int timer);
+	void clear();
+private:
+	void threadProcessInterval(std::function<void()> handler,int timeout);
+	void threadProcessTimeout(std::function<void()> handler,int timeout);
 private:
 	bool active;
 	std::mutex m;
 	std::condition_variable cv;
-	void threadProcessInterval(std::function<void()> handler,int timeout)
-	{
-		while(active){
-			std::unique_lock<std::mutex> lock(m);
-			auto status = cv.wait_for(lock,std::chrono::milliseconds(timeout));
-			if (status == std::cv_status::timeout){
-				if(!active) return;
-				if (handler != nullptr)
-					handler();
-			}
-		}
-		
-	}
-	void threadProcessTimeout(std::function<void()> handler,int timeout)
-	{
-			std::unique_lock<std::mutex> lock(m);
-			auto status = cv.wait_for(lock,std::chrono::milliseconds(timeout));
-			if (status == std::cv_status::timeout){
-				if(!active) return;
-				if (handler != nullptr)
-					handler();
-			}
-	}
-public:
-	void setInterval(std::function<void()> ptr, int timer)
-	{
-		active = true;
-		std::thread t(([=] {
-			threadProcessInterval(ptr,timer);
-			}));
-		t.detach();
-		
-	}
-	void setTimeout(std::function<void()> ptr, int timer)
-	{	
-		active = true;
-		std::thread t(([=] {
-			threadProcessTimeout(ptr,timer);
-			}));
-		t.detach();
-		
-	}
-	void clear(){
-		active = false;
-		cv.notify_all();
-	}
 };
+
+inline void ThreadTimer::threadProcessInterval(std::function<void()> handler,int timeout)
+{
+	while(active){
+		std::unique_lock<std::mutex> lock(m);
+		auto status = cv.wait_for(lock,std::chrono::milliseconds(timeout));
+		if (status == std::cv_status::timeout){
+			if(!active) return;
+			if (handler != nullptr)
+				handler();
+		}
+	}
+	
+}
+inline void ThreadTimer::threadProcessTimeout(std::function<void()> handler,int timeout)
+{
+		std::unique_lock<std::mutex> lock(m);
+		auto status = cv.wait_for(lock,std::chrono::milliseconds(timeout));
+		if (status == std::cv_status::timeout){
+			if(!active) return;
+			if (handler != nullptr)
+				handler();
+		}
+}
+
+inline void ThreadTimer::setInterval(std::function<void()> ptr, int timer)
+{
+	active = true;
+	std::thread t(([=] {
+		threadProcessInterval(ptr,timer);
+		}));
+	t.detach();
+	
+}
+inline void ThreadTimer::setTimeout(std::function<void()> ptr, int timer)
+{	
+	active = true;
+	std::thread t(([=] {
+		threadProcessTimeout(ptr,timer);
+		}));
+	t.detach();
+	
+}
+inline void ThreadTimer::clear(){
+	active = false;
+	cv.notify_all();
+}
 
 #endif //THREAD_TIMER_H
