cf14bc520044e5b9ce52fc3dd1409b1d1d2466f5
JelleD <jelledriessen2003@gmail.com>
2024-06-05 13:49:05 +0200

    Info page styling now consistent + version numbers

--- frontend/src/routes/InfoRoute.vue
+++ frontend/src/routes/InfoRoute.vue
@@ -1,23 +1,29 @@
 <script lang="ts" setup>
 import { isMobile } from '@/util/detectmobilebrowser'
+import { useViewStore } from '@/stores/viewStore'
+import { storeToRefs } from 'pinia';
+
+const viewStore = useViewStore()
+const { versionNumber } = storeToRefs(viewStore)
 </script>
 <template>
     <div v-if="isMobile()">
-        <div class="w-full h-full grid grid-rows-4 grid-cols-1 justify-center">
-            <div class="m-auto row-start-1 row-span-3 w-full mt-16">
-                <img src="./../../public/assets/info-phone.svg" />
+        <div class="h-full w-full grid grid-rows-8 grid-cols-1 justify-center">
+            <div class="row-start-1 row-span-7 max-h contain-content">
+                <img class="h-full w-full" src="./../../public/assets/info-phone.svg" />
             </div>
-            <div class="m-auto mb-8 row-start-4">
+            <div class="m-auto row-start-8 justify-center text-center">
                 <router-link to="/join" class="underline mr-4 text-2xl">Join a party!</router-link> 
+                <p class="mt-8">{{ versionNumber }}</p>
             </div>
         </div>
     </div>
     <div v-else>
-        <div class="w-full h-full grid grid-rows-4 grid-cols-1 justify-center">
-            <div class="m-auto row-start-1 row-span-3 w-2/3 mt-16">
-                <img src="./../../public/assets/info-desktop.svg" />
+        <div class="w-full h-full grid grid-rows-8 grid-cols-1 justify-center">
+            <div class="row-start-1 row-span-7 max-h contain-content">
+                <img class="h-full w-full" src="./../../public/assets/info-desktop.svg" />
             </div>
-            <div class="m-auto mb-8 row-start-4">
+            <div class="m-auto mb-8 row-start-8">
                 <router-link to="/join" class="underline mr-16 text-4xl">Join a party!</router-link>
                 <router-link to="/host" class="underline ml-16 text-4xl">Host a party!</router-link>
             </div>
