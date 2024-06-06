429584946124a6679d2e9f3daf6c5856ab76c1a5
Krijn Grimme <krijngrimme@hotmail.com>
2024-06-06 15:34:51 +0200

    Somewhat working results screen

--- frontend/src/util/funcs.ts
+++ frontend/src/util/funcs.ts
@@ -13,3 +13,11 @@ export function formatMilliseconds(miliseconds: number) {
   const seconds = ((miliseconds % 60000) / 1000).toFixed(0)
   return `${minutes}:${Number(seconds) < 10 ? '0' : ''}${seconds}`
 }
+
+export function unLerp(start: number, end: number, t: number) {
+  return (t - start) / (end - start)
+}
+
+export function lerp(start: number, end: number, t: number) {
+  return start * (1 - t) + end * t
+}
