fd36ad577ba8b9c2c003e6ae72dbd53a59c31da1
JelleD <70947553+JCDriessen@users.noreply.github.com>
2025-10-31 09:44:13 +0100

    Change order to fix docker build

--- backend/src/websocket.cpp
+++ backend/src/websocket.cpp
@@ -126,6 +126,15 @@ void WebSocket::init() {
       .ws<SocketData>(
           "/host",
           {/* Handlers */
+           .upgrade =
+               [](auto *res, uWS::HttpRequest * req, auto *context) {
+                 // 'req->getHeader("host")' outputs on local 'localhost:localhost:5173'
+                 res->template upgrade<SocketData>(
+                     {.isLocalhost = req->getHeader("host").find("localhost") != std::string::npos},
+                     req->getHeader("sec-websocket-key"),
+                     req->getHeader("sec-websocket-protocol"),
+                     req->getHeader("sec-websocket-extensions"), context);
+               },
            .open =
                [](auto *ws) {
                  std::cout << "connection started with host" << std::endl;
@@ -140,15 +149,6 @@ void WebSocket::init() {
 
                  send_host_message(ws);
                  p->start_game();
-               },
-           .upgrade =
-               [](auto *res, uWS::HttpRequest * req, auto *context) {
-                 // 'req->getHeader("host")' outputs on local 'localhost:5173'
-                 res->template upgrade<SocketData>(
-                     {.isLocalhost = req->getHeader("host").find("localhost") != std::string::npos},
-                     req->getHeader("sec-websocket-key"),
-                     req->getHeader("sec-websocket-protocol"),
-                     req->getHeader("sec-websocket-extensions"), context);
                },
            .message =
                [](auto *ws, std::string_view message, uWS::OpCode opCode) {
